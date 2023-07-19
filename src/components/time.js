c58a634722c434b8b15c3bd969cdb3dd44f47423
Mariya-go <mariya.golovko@gmail.com>
2023-07-19 09:40:37 +0200

    update index.html, time.js, style css

--- src/components/time.js
+++ src/components/time.js
@@ -1,36 +1,37 @@
 import { data } from "../data.js";
+import { dom } from "../data.js";
 
 const createTimeComponent = () => {
-  const time = document.createElement("div");
-  time.className = "time";
+  // const time = document.createElement("div");
+  // time.className = "time";
 
-  const minutes = document.createElement("span");
-  minutes.className = "minutes";
+  // const minutes = document.createElement("span");
+  // minutes.className = "minutes";
   if (data.minutes < 10) {
-    minutes.innerText = "0" + data.minutes + " : ";
+    dom.minutes.innerText = "0" + data.minutes + " : ";
   } else if (data.minutes >= 10) {
-    minutes.innerText = data.minutes + ":";
+    dom.minutes.innerText = data.minutes + ":";
   }
 
-  const seconds = document.createElement("span");
-  seconds.className = "seconds";
+  // const seconds = document.createElement("span");
+  // seconds.className = "seconds";
   if (data.seconds < 10) {
-    seconds.innerText = "0" + data.seconds + " : ";
+    dom.seconds.innerText = "0" + data.seconds + " : ";
   } else if (data.seconds >= 10) {
-    seconds.innerText = data.seconds + ":";
+    dom.seconds.innerText = data.seconds + ":";
   }
 
-  const milliseconds = document.createElement("span");
-  milliseconds.className = "milliseconds";
+  // const milliseconds = document.createElement("span");
+  // milliseconds.className = "milliseconds";
   if (data.milliseconds < 10) {
-    milliseconds.innerText = "00" + data.milliseconds;
+    dom.milliseconds.innerText = "00" + data.milliseconds;
   } else if (data.milliseconds < 100) {
-    milliseconds.innerText = "0" + data.milliseconds;
+    dom.milliseconds.innerText = "0" + data.milliseconds;
   } else {
-    milliseconds.innerText = data.milliseconds;
+    dom.milliseconds.innerText = data.milliseconds;
   }
 
-  time.append(minutes, seconds, milliseconds);
+  dom.time.append(minutes, seconds, milliseconds);
 
-  return time;
+  return dom.time;
 };
--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
-const data = 
-{
-    minutes      : 0,
-    seconds      : 0,
-    milliseconds : 0,   
-    watchInterval: null,   
-}
+export const data = {
+  minutes: 0,
+  seconds: 0,
+  milliseconds: 0,
+  watchInterval: null,
+};
 
-export default data;
+export const dom = {
+  time: document.getElementById("time"),
+  startButton: document.getElementById("start"),
+  stopButton: document.getElementById("stop"),
+  resetButton: document.getElementById("reset"),
+  minutes: document.querySelector(".minutes"),
+  seconds: document.querySelector(".seconds"),
+  milliseconds: document.querySelector(".milliseconds"),
+};
